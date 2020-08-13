3d485008345b5a621c0a55918e9def005e89e00c
Yura-by <s.older681@gmail.com>
2020-08-13 18:13:30 +0300

    Добавит первую разметку

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
-const App = () => {
-  return <div>Hello World!</div>
-};
+import App from './components/app/app';
 
 ReactDOM.render(<App/>, document.getElementById(`root`));
